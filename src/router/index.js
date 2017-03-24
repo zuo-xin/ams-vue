e35ecbf844e14212c9da38e1c7480635f82d389a
zuoxin <zuox1224@hotmail.com>
2017-03-24 22:36:45 +0800

    fix bug

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,7 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '*',
+      path: '/',
       component: Home,
       children:[
         { path: '/', component: homeCheck},
